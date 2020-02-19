0434e3f7c805e855121cc1d6f753f8d55fa56771
unknown <Administrator@SILAB-27.ASIETDomain.com>
2020-02-19 15:28:53 +0530

    new

--- src/container/cartcontainer.jsx
+++ src/container/cartcontainer.jsx
@@ -1,12 +1,41 @@
 import React from "react";
+import productTile from "../components/productTile";
+import {Link} from "react-router-dom";
+
 
 export default class Cart extends React.Component {
     
+    constructor(props){
+        super(props);
+        this.state={
+            cart:[]
+        }
+    }
+    componentsDidMount(){
+        this.initCart();
+    }
+    initCart(){
+        let myCart=localStorage.getItem('cart')
+        myCart=JSON.parse(myCart)
+        this.setState({
+            cart: myCart || []
+        })
+    }
+    clearCart() {
+        localStorage.setItem('cart', JSON.stringify([]))
+        this.setState({
+            cart: []
+        })
+    }
+
     render()
     {
+        const productsList=this.state.cart.map((product)=> {
+            return productTile(null,product)
+        })
         return(
         <div>
-           My Cart!!
+           {productsList}
         </div>
         )
     }
